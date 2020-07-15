3cced775098000f90e50de9dc2184a58174f7909
Abhishek-Mahapatra <61933686+Abhishek-Mahapatra@users.noreply.github.com>
2020-07-16 00:59:16 +0530

    Create backspace-string-compare(09).cpp

---
+++ backspace-string-compare(09).cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void remove(string& s, int& len) {
+        for(int i = 0; i < (int) s.length(); ++i) {
+            if(s[i] == '#') {
+                if(len > 0) {
+                    len--;
+                }
+            }
+            else {
+                s[len] = s[i];
+                len++;
+            }
+        }
+    }
+public:
+    bool backspaceCompare(string S, string T) {
+        int len_s = 0, len_t = 0;
+        remove(S, len_s);
+        remove(T, len_t);
+        if(len_s != len_t) {
+            return false;
+        }
+        for(int i = 0; i < len_s; ++i) {
+            if(S[i] != T[i]) {
+                return false;
+            }
+        }
+        return true;
+        // return len_s == len_t && S == T;
+    }
+};
